c7ac5bfecda1638b8f54271a9e8ca94a044a1e30
nasrinette <97260770+nasrinette@users.noreply.github.com>
2023-04-03 14:59:02 +0400

    projects

---
+++ src/Components/Projects/data.js
@@ -0,0 +1,109 @@
+import img1 from "./assets/SuretKargo.png";
+import img2 from "./assets/Trinity.png";
+import img3 from './assets/Yoga.png'
+import img4 from './assets/Mind.png'
+import img5 from './assets/travel.png'
+import img6 from './assets/Crypto.png'
+import img7 from './assets/parkcinema.png'
+import img8 from './assets/Doodle.png'
+import img9 from './assets/gpt3.png'
+import img10 from './assets/passwd.png'
+
+export const projects = [
+  {
+    id: 1,
+    title: "Suret Kargo",
+    description: "It is a website for a cargo service in Azerbaijan.",
+    image: img1,
+    technologies: ["React", "CSS", "Figma", "HTML"],
+    liveLink: "https://www.suretkargo.az/#",
+    sourceLink:
+      "https://www.figma.com/file/zccqPacXHrgVyUb6STkeBa/Surat-Kargo?node-id=0%3A1&t=rjItxJNDY2sQSLwd-1",
+  },
+  {
+    id: 2,
+    title: "Trinity private school",
+    description: "It is a website and LMS system for a private school",
+    image: img2,
+    technologies: ["HTML", "CSS", "React", "Figma"],
+    liveLink: "https://trinity.cy",
+    sourceLink:
+      "https://www.figma.com/file/yheEgJXrZsAAljDhTpTGqy/LMS-for-Trinity?node-id=0%3A1&t=M8KK7UTa0Kk67AIE-1",
+  },
+  {
+    id: 3,
+    title: "ProYoga Buddy",
+    description: "A website to find yoga partner",
+    image: img3,
+    technologies: ["HTML", "CSS", "React", "Figma"],
+    liveLink: "/",
+    sourceLink:
+      "https://www.figma.com/proto/hpF9t1NYW8YY7gTZ5CT3E5/YogaApp?node-id=35%3A261&scaling=min-zoom&page-id=35%3A108&starting-point-node-id=35%3A261&fuid=1110271128923199998",
+  },
+  {
+    id: 4,
+    title: "The Mind Game",
+    description: "It is a website for digital version of a table game",
+    image: img4,
+    technologies: ["Figma"],
+    liveLink: "/",
+    sourceLink:
+      "https://www.figma.com/file/yheEgJXrZsAAljDhTpTGqy/LMS-for-Trinity?node-id=0%3A1&t=M8KK7UTa0Kk67AIE-1",
+  },
+  {
+    id: 5,
+    title: "Travel Agency",
+    description: "Web design for a travel agency that provides world tours",
+    image: img5,
+    technologies: ["Figma"],
+    liveLink: "/",
+    sourceLink:"https://www.figma.com/proto/mJKOIUjVJzEz71dwwkCH38/Travel%2BWebsite?node-id=29%3A219&scaling=min-zoom&page-id=29%3A81" 
+},
+{
+  id: 6,
+  title: "CryptoMoney",
+  description: "Web design for a platform for Investing in Crypto and Manage Your Finance",
+  image: img6,
+  technologies: ["Figma"],
+  liveLink: "/",
+  sourceLink:"https://www.figma.com/proto/bMTy61JBeTOy9eRWru6Xyb/CryptoMoney?node-id=201%3A436&scaling=min-zoom&page-id=201%3A58" 
+},
+{
+  id: 7,
+  title: "Park Cinema",
+  description: "It is a mobile app design for a cinema in Azerbaijan.",
+  image: img7,
+  technologies: ["Figma"],
+  liveLink: "https://play.google.com/store/apps/details?id=az.parkcinema.app&hl=en&gl=US&pli=1",
+  sourceLink:"https://www.figma.com/file/KprDRcYI21CHDGdnFbKWJz/parkcinema?node-id=0%3A1&t=EdFA0ZrRY5QeoZR5-1" 
+},
+{
+  id: 8,
+  title: "Doodle",
+  description: "Simple photo searching app with Unsplash API",
+  image: img8,
+  technologies: ["CSS, HTML, React"],
+  liveLink: "https://search-pics-172317.netlify.app",
+  sourceLink:"https://github.com/nasrinette/Photos-search-app" 
+},
+{
+    id: 9,
+    title: "GPT-3",
+    description: "Static web page with appealing design",
+    image: img9,
+    technologies: ["CSS, HTML, React, Javascript"],
+    liveLink: "https://what-is-gpt-3.netlify.app",
+    sourceLink:"https://github.com/nasrinette/gpt3-ui-ux-static-site" 
+  },
+  {
+    id: 10,
+    title: "Password generator",
+    description: "Random password generator",
+    image: img10,
+    technologies: ["CSS, HTML, Javascript"],
+    liveLink: "https://paswd-generator.netlify.app",
+    sourceLink:"https://github.com/nasrinette/Password-generator" 
+  },
+
+
+];
